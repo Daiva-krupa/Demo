9bb5f77fb370a58b7f587dbbf6630951a868b82a
Daiva-krupa <121303453+Daiva-krupa@users.noreply.github.com>
2024-01-08 12:24:11 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,28 +1,24 @@
 import streamlit as st
 st.set_page_config(page_title='Dogs')
 st.header("Types of Dogs")
+st.subheader("Retriever Dog")
+st.image("./Retriver.jpg",width=200,use_column_width=True)
+st.write("Retriver dogs are honest and faithfull")
+
+st.subheader("Shitzu Dog")
+st.image("./Shitzu.jpg",width=200,use_column_width=True)
+st.write("Shitzu dogs are small and cute")
+
+st.subheader("Bull Dog")
+st.image("./Bulldog.jpg",width=200,use_column_width=True)
+st.write("Bull dogs are much aggressive")
 
 
-
-  st.subheader("Retriever Dog")
-  st.image("./Retriver.jpg",width=200,use_column_width=True)
-  st.write("Retriver dogs are honest and faithfull")
-
-
-  st.subheader("Shitzu Dog")
-  st.image("./Shitzu.jpg",width=200,use_column_width=True)
-  st.write("Shitzu dogs are small and cute")
-
-  st.subheader("Bull Dog")
-  st.image("./Bulldog.jpg",width=200,use_column_width=True)
-  st.write("Bull dogs are much aggressive")
+st.subheader("Alaskan Dog")
+st.image("./Alaskan.jpg",width=200,use_column_width=True)
+st.write("Alaskans are intelligent and active")
 
 
-  st.subheader("Alaskan Dog")
-  st.image("./Alaskan.jpg",width=200,use_column_width=True)
-  st.write("Alaskans are intelligent and active")
-
-
-  st.subheader("Beagle Dog")
-  st.image("./Beagle.jpg",width=200,use_column_width=True)
-  st.write("Beagles are loving and lovable")
+st.subheader("Beagle Dog")
+st.image("./Beagle.jpg",width=200,use_column_width=True)
+st.write("Beagles are loving and lovable")
