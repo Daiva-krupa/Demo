aff869bd3491e0953ab78dbe6dafa3dcf86b1ace
Daiva-krupa <121303453+Daiva-krupa@users.noreply.github.com>
2024-01-08 12:18:36 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -5,27 +5,27 @@ st.header("Types of Dogs")
 col1,col2,col3,col4=st.columns(4)
 with col1:
   st.subheader("Retriever Dog")
-  st.image("./Retriver.jpg",width=600,use_column_width=False)
+  st.image("./Retriver.jpg",width=200,use_column_width=False)
   st.write("Retriver dogs are honest and faithfull")
 
 with col2:
   st.subheader("Shitzu Dog")
-  st.image("./Shitzu.jpg",width=500,use_column_width=False)
+  st.image("./Shitzu.jpg",width=200,use_column_width=False)
   st.write("Shitzu dogs are small and cute")
 
 with col3:
   st.subheader("Bull Dog")
-  st.image("./Bulldog.jpg",width=600,use_column_width=False)
+  st.image("./Bulldog.jpg",width=200,use_column_width=False)
   st.write("Bull dogs are much aggressive")
 
 
 with col3:
   st.subheader("Alaskan Dog")
-  st.image("./Alaskan.jpg",width=600,use_column_width=False)
+  st.image("./Alaskan.jpg",width=200,use_column_width=False)
   st.write("Alaskans are intelligent and active")
 
 
 with col4:
   st.subheader("Beagle Dog")
-  st.image("./Beagle.jpg",width=600,use_column_width=False)
+  st.image("./Beagle.jpg",width=200,use_column_width=False)
   st.write("Beagles are loving and lovable")
