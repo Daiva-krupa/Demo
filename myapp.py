a67312fb67f45e062e9b3a49b2c70e569ee46d6c
Daiva-krupa <121303453+Daiva-krupa@users.noreply.github.com>
2024-01-08 12:00:56 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,8 +1,8 @@
 import streamlit as st
-st.set_page_config(page_title='Cats')
-st.header("types of Cats")
+st.set_page_config(page_title='Dogs')
+st.header("types of Dogs")
 
-col1,col2=st.columns(2)
+col1,col2,col3=st.columns(3)
 with col1:
   st.subheader("Retriever Dog")
   st.image("./Retriver.jpg",width=500,use_column_width=True)
@@ -12,3 +12,8 @@ with col2:
   st.subheader("Shitzu Dog")
   st.image("./Shitzu.jpg",,width=500,use_column_width=True)
   st.write("Shitzu dogs are small and cute")
+
+with col3:
+  st.subheader("Bull Dog")
+  st.image("./Bull.jpg",,width=500,use_column_width=True)
+  st.write("Bull dogs are much aggressive")
