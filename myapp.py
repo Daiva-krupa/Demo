efcd915c2e4432e4662abbc2b27b702d57a11293
Daiva-krupa <121303453+Daiva-krupa@users.noreply.github.com>
2024-01-08 11:47:03 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -5,10 +5,10 @@ st.header("types of Cats")
 col1,col2=st.columns(2)
 with col1:
   st.subheader("Retriever Dog")
-  st.image("./Retriver.jpg")
+  st.image("./Retriver.jpg",width=500,use_column_width=True)
   st.write("Retriver dogs are honest and faithfull")
 
 with col2:
   st.subheader("Shitzu Dog")
-  st.image("./Shitzu.jpg")
+  st.image("./Shitzu.jpg",,width=500,use_column_width=True)
   st.write("Shitzu dogs are small and cute")
