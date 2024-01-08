1c0897189f8a0bfdd0a885fd30a75365d4d2c363
Daiva-krupa <121303453+Daiva-krupa@users.noreply.github.com>
2024-01-08 12:17:56 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -5,17 +5,17 @@ st.header("Types of Dogs")
 col1,col2,col3,col4=st.columns(4)
 with col1:
   st.subheader("Retriever Dog")
-  st.image("./Retriver.jpg",width=600,use_column_width=True)
+  st.image("./Retriver.jpg",width=600,use_column_width=False)
   st.write("Retriver dogs are honest and faithfull")
 
 with col2:
   st.subheader("Shitzu Dog")
-  st.image("./Shitzu.jpg",width=500,use_column_width=True)
+  st.image("./Shitzu.jpg",width=500,use_column_width=False)
   st.write("Shitzu dogs are small and cute")
 
 with col3:
   st.subheader("Bull Dog")
-  st.image("./Bulldog.jpg",width=600,use_column_width=True)
+  st.image("./Bulldog.jpg",width=600,use_column_width=False)
   st.write("Bull dogs are much aggressive")
 
 
@@ -27,5 +27,5 @@ with col3:
 
 with col4:
   st.subheader("Beagle Dog")
-  st.image("./Beagle.jpg",width=600,use_column_width=True)
+  st.image("./Beagle.jpg",width=600,use_column_width=False)
   st.write("Beagles are loving and lovable")
